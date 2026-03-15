19be4e9138670c0bd7d9954f6946daa96eb3e253
arsen <arsenulykbekov9@gmail.com>
2026-03-15 17:51:46 +0500

    feature(main): fix runtime-config.sh

--- docker/runtime-config.sh
+++ docker/runtime-config.sh
@@ -5,6 +5,7 @@ CONFIG_PATH="/usr/share/nginx/html/config.js"
 
 cat > "$CONFIG_PATH" <<EOF
 window.__APP_CONFIG__ = {
-  VITE_API_URL: "${VITE_API_URL:-}"
+  VITE_API_URL: "${VITE_API_URL:-}",
+  VITE_PARSER_API_URL: "${VITE_PARSER_API_URL:-}"
 };
 EOF
